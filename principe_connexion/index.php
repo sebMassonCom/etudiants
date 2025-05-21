a2684b266c4461c175d6c33668aa34b51b4fdf1d
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-21 17:33:20 +0200

    ajout de la page index.php
    ajout de la page index.php

---
+++ principe_connexion/index.php
@@ -0,0 +1,10 @@
+<?php
+include 'assets/inc/header.php';
+$page = filter_input(INPUT_GET, 'page');
+if(file_exists('controllers/'.$page.'.class.php')){
+    require_once 'controllers/'.$page.'.class.php';
+    new $page;
+} else {
+    require_once 'controllers/404.php';
+    new P404;
+}

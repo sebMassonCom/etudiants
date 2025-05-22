f3228afb49c6f8ca03192357601125bf6f93d883
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-22 09:34:06 +0200

    ajout des pages model avec etudiant et admin
    ajout des pages model avec etudiant et admin

--- principe_connexion/models/modelspromo_admin.php
+++ principe_connexion/models/modelspromo_admin.php
@@ -1,26 +1,24 @@
 <?php
 require_once 'core/Database.php';
 
-class Modelpromo_etudiant{
+
+class Modelpromo_admin{
     private $_db;
 
     public function __construct() {
         $this->_db = new Database();
     }
 
-    public function connexionetu($nom, $ecole, $mot_de_passe){
-        $sql = Database::$db->prepare("INSERT INTO user(ecole, mot_de_passe) VALUES (:ecole,:mot_de_passe)");
+    public function connexionad($nom, $ecole, $mot_de_passe){
+        $sql = Database::$db->prepare("INSERT INTO user(nom,ecole, mot_de_passe) VALUES (:nom,:ecole,:mot_de_passe)");
+        $sql->bindParam(':nom',$nom);
         $sql->bindParam(':ecole',$ecole);
         $sql->bindParam(':mot_de_passe',$mot_de_passe);
         $sql->execute();
         foreach($user AS $u){
             if(password_verify($mot_de_passe, $u['mot_de_passe'])){
                 echo 'Vous êtes connecté';
-            } 
-        }
+            }
+        } 
     }
 }
-
-
-
-

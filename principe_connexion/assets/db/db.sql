a7e887076ede81f29c47d7879c3fe3daf935b9f8
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-22 09:44:45 +0200

    Create db.sql

---
+++ principe_connexion/assets/db/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE espace_etudiant;
+
+USE espace_etudiant;
+
+CREATE TABLE  users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255),
+    ecole VARCHAR(50),
+    mot_de_passe VARCHAR(100)
+);
+
+CREATE TABLE promotions(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ecole VARCHAR(50),
+    mot_de_passe VARCHAR(100)
+);

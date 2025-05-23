b6c82b6fb0386d7d2e670d2f93b3fe95ced83375
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-23 10:53:22 +0200

    ajout du dossier header

---
+++ connexion_promo_ecole/assets/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>espace_etudiant</title>
+</head>
+<body>
+    <header>
+        <a href="index.php?page=accueil"><h1>Page accueil</h1></a>
+    </header>
+    <nav>
+        <a href="index.php?page=Promo_admin">Création promotions</a> |
+        <a href="index.php?page=Promo_etudiant">Connexion Étudiant</a>
+    </nav>
+</body>
+</html>

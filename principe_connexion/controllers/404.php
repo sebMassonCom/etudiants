87e59c247f452169ddebb49408425cfd33dde222
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-23 10:48:07 +0200

    Delete principe_connexion directory

--- principe_connexion/controllers/404.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class P404{
-    
-    public function __construct(){
-        include 'views/404.php';
-    }
-}
--- principe_connexion/controllers/accueil.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Accueil {
-    public function __construct() {
-        include 'views/accueil.php';
-    }
-}

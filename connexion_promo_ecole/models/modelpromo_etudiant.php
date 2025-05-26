edc0138142613abdb2edff31dd6d05b50914a821
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-26 17:09:33 +0200

    commit model

--- connexion_promo_ecole/models/modelpromo_etudiant.php
+++ connexion_promo_ecole/models/modelpromo_etudiant.php
@@ -1,20 +1,60 @@
 <?php
 require_once 'core/Database.php';
 
-class Modelpromo_etudiant{
+class Modelpromo_etudiant {
     private $_db;
 
     public function __construct() {
         $this->_db = new Database();
     }
 
-    public function espaceecole($ecole, $mot_de_passe){
-        $sql = Database::$db->prepare("INSERT INTO users(ecole, mot_de_passe) VALUES (:ecole,:mot_de_passe)");
-        $sql->bindParam(':ecole',$ecole);
-        $sql->bindParam(':mot_de_passe',$mot_de_passe);
+    public function afficherecole() {
+        return Database::$db->query("SELECT * FROM ecole");
+    }
+
+    public function afficherpromotion() {
+        return Database::$db->query("SELECT * FROM promotion");
+    }
+
+    public function espaceecole($n, $mdp, $p, $e) {
+        $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
+
+        $sql = Database::$db->prepare("INSERT INTO users(ecole, promotion, mot_de_passe, nom) VALUES (:ecole, :promotion, :mot_de_passe, :nom )");
+        $sql->bindParam(':ecole', $e);
+        $sql->bindParam(':nom', $n);
+        $sql->bindParam(':promotion', $p);
+        $sql->bindParam(':mot_de_passe', $hashed_password);
         $sql->execute();
     }
+
+    public function verifierMotDePasse($n, $mdp) {
+        $user = Database::$db->query("SELECT * FROM users WHERE nom='$n' LIMIT 1");
+        $user->execute();
+
+        foreach($user AS $u){
+        
+        if(password_verify($mdp, $u['mot_de_passe']())){
+
+        $_SESSION['nom'] = $u['nom'];
+        $_SESSION['mot_de_passe'] = $u['mot_de_passe'];
+        echo 'connexion reussie';
+    } else {
+        $_SESSION['message'] = "Erreur";
+        header('Location:./views/404.php');
+        exit;
+    }
+    }
+        
+    }
 }
+
+
+
+
+
+
+
+
 
 
 

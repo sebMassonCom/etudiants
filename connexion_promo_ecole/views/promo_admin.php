78202a3da93e3040fdeb2379e3f8b08ee658d152
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-26 17:10:09 +0200

    push views

--- connexion_promo_ecole/views/promo_admin.php
+++ connexion_promo_ecole/views/promo_admin.php
@@ -1,25 +1,59 @@
 <?php
 
-if(filter_input(INPUT_POST,'nom',FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
-    if($_POST['nom'] && $_POST['ecole'] && $_POST['mot_de_passe']){
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+if(filter_input(INPUT_POST,'nom')){
         $nom = filter_input(INPUT_POST,'nom',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $promotion = filter_input(INPUT_POST, 'promotion', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $ecole = filter_input(INPUT_POST,'ecole',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $mot_de_passe = filter_input(INPUT_POST,'mot_de_passe',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $hashpass = password_hash($_POST['mot_de_passe'],PASSWORD_BCRYPT);
-        $this->connexionadmin(new Admin($nom,$ecole,$hashpass));
-        echo 'Création de la promo effectuer ';
+        
+        $admin = new Admin($nom, $promotion, $ecole, $hashpass);
+        $this->connexionadmin($admin);
+        $_SESSION['admin'] = $admin->get_nom();
+
+
+        
+        if ($admin && password_verify($mot_de_passe, $admin->get_mot_de_passe())) {
+        $_SESSION['admin'] = $admin->getNom();
+        $_SESSION['token'] = uniqid();
+        $_SESSION['ecole'] = $ecole;
+
+        echo 'Connexion promo';
     } else {
-        echo 'Identifiants et/ou mot de passe incorrect !';
+        $_SESSION['message'] = "Identifiants et/ou mot de passe incorrect !";
+        header('Location:./assets/inc/header.php');
+        exit;
     }
 }
 ?>
-<h2>Création d'une promo</h2>
-<form method="POST">
-    <input type="text" name="nom" placeholder="Indiquer le nom de la promo">
+<h2>connexion Admin</h2>
+<form method="POST">                                                                             
+    <select name="ecole" id="ecole" >
+        <option disabled selected>nom de l'ecole</option>
+        <?php
+        foreach($afficherecole AS $e):
+            ?>
+        <option value="<?=$e['id']?>"><?=$e['nom_ecole']?></option>
+        <?php endforeach; ?>
+    </select>
     <br>
-    <input type="text" name="ecole" placeholder="Indiquer l'ecole">
+    <select name="promotion" id="promotion" >
+        <option disabled selected>promotion</option>
+        <?php
+        foreach($afficherpromotion AS $p):
+            ?>
+        <option value="<?=$p['id']?>"><?=$p['nom_promotion']?></option>
+        <?php endforeach; ?>
+    </select>
     <br>
-    <input type="password" name="mot_de_passe" placeholder="Indiquer le Mot de passe">
+    <input type="text" name="nom" placeholder="Indiquer votre nom">
     <br>
-    <input type="submit" value="Creation de la promo">
+    <input type="password" name="mot_de_passe" placeholder="Indiquer votre Mot de passe">
+    <br>
+    <input type="submit" value="Identification">
 </form>
--- connexion_promo_ecole/views/promo_etudiant.php
+++ connexion_promo_ecole/views/promo_etudiant.php
@@ -1,19 +1,56 @@
 <?php
 
-if(filter_input(INPUT_POST,'ecole',FILTER_SANITIZE_FULL_SPECIAL_CHARS)){
-    if($_POST['ecole'] && $_POST['mot_de_passe']){
-        $ecole = filter_input(INPUT_POST,'ecole',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $mot_de_passe = filter_input(INPUT_POST,'mot_de_passe',FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $this->connexionetudiant(new Etudiant($ecole,$mot_de_passe));
-        echo 'Vous êtes connecté ';
+session_start();
+
+if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+if (filter_input(INPUT_POST, 'ecole')) {
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $ecole = filter_input(INPUT_POST, 'ecole', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $promotion = filter_input(INPUT_POST, 'promotion', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $mot_de_passe = filter_input(INPUT_POST, 'mot_de_passe', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $hashpass = password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT);
+
+    $etudiant = new Etudiant($promotion, $nom, $ecole, $hashpass);
+
+    if ($etudiant && password_verify($mot_de_passe, $etudiant->get_mot_de_passe())) {
+        $_SESSION['etudiant'] = $etudiant->get_nom();
+        $_SESSION['token'] = uniqid();
+        $_SESSION['ecole'] = $ecole;
+
+        echo 'connexion reussie ';
     } else {
-        echo 'Identifiants et/ou mot de passe incorrect !';
+        $_SESSION['message'] = "Identifiants et/ou mot de passe incorrect ";
+        header('Location: ./assets/inc/header.php');
+        exit;
     }
 }
 ?>
+
 <h2>Connexion Étudiant</h2>
-<form method="POST">
-    <input type="text" name="ecole" placeholder="Indiquer votre ecole">
+<form method="POST">                                                                             
+    <select name="ecole" id="ecole" >
+        <option disabled selected>nom de l'ecole</option>
+        <?php
+        foreach($afficherecole AS $e):
+            ?>
+        <option value="<?=$e['id']?>"><?=$e['nom_ecole']?></option>
+        <?php endforeach; ?>
+    </select>
+    <br>
+    <select name="promotion" id="promotion" >
+        <option disabled selected>promotion</option>
+        <?php
+        foreach($afficherpromotion AS $p):
+            ?>
+        <option value="<?=$p['id']?>"><?=$p['nom_promotion']?></option>
+        <?php endforeach; ?>
+    </select>
+    <br>
+    <input type="text" name="nom" placeholder="Indiquer votre nom">
     <br>
     <input type="password" name="mot_de_passe" placeholder="Indiquer votre Mot de passe">
     <br>

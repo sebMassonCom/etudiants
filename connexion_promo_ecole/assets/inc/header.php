4755fa5fd743ac77b92948af05f182b8c63c7218
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-26 17:11:15 +0200

    push header

--- connexion_promo_ecole/assets/inc/header.php
+++ connexion_promo_ecole/assets/inc/header.php
@@ -9,7 +9,7 @@
         <a href="index.php?page=accueil"><h1>Page accueil</h1></a>
     </header>
     <nav>
-        <a href="index.php?page=Promo_admin">Création promotions</a> |
+        <a href="index.php?page=Promo_admin">Connexion admin</a> |
         <a href="index.php?page=Promo_etudiant">Connexion Étudiant</a>
     </nav>
 </body>

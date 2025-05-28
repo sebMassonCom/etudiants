14f51f1f0f037069f382a3cad1ddcd175d0a7f29
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-28 08:51:25 +0200

    push models

--- connexion_promo_ecole/models/modelpromo_etudiant.php
+++ connexion_promo_ecole/models/modelpromo_etudiant.php
@@ -16,31 +16,37 @@ class Modelpromo_etudiant {
         return Database::$db->query("SELECT * FROM promotion");
     }
 
-    public function espaceecole($n, $mdp, $p, $e) {
+    public function espaceecole($p,$n,$e,$mdp) {
         $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
 
-        $sql = Database::$db->prepare("INSERT INTO users(ecole, promotion, mot_de_passe, nom) VALUES (:ecole, :promotion, :mot_de_passe, :nom )");
-        $sql->bindParam(':ecole', $e);
+        $sql = Database::$db->prepare("INSERT INTO users(id_promotion,nom,id_ecole,mot_de_passe ) VALUES (:id_promotion,:nom,:id_ecole,:mot_de_passe)");
+        $sql->bindParam(':id_promotion', $p);
         $sql->bindParam(':nom', $n);
-        $sql->bindParam(':promotion', $p);
+        $sql->bindParam(':id_ecole', $e);
         $sql->bindParam(':mot_de_passe', $hashed_password);
         $sql->execute();
     }
 
-    public function verifierMotDePasse($n, $mdp) {
-        $user = Database::$db->query("SELECT * FROM users WHERE nom='$n' LIMIT 1");
+    public function afficher(){
+        return Database::$db->query("SELECT * FROM users INNER JOIN promotion ON users.id_promotion = promotion.id INNER JOIN ecole ON users.id_ecole = ecole.id");
+    }
+
+    public function verifierMotDePasse($mdp) {
+        $user = Database::$db->query("SELECT * FROM users WHERE nom='$mdp' LIMIT 1");
         $user->execute();
 
         foreach($user AS $u){
-        
-        if(password_verify($mdp, $u['mot_de_passe']())){
+        // var_dump($u); break; exit();
+        if(password_verify($mdp, $u['mot_de_passe'])){
 
         $_SESSION['nom'] = $u['nom'];
         $_SESSION['mot_de_passe'] = $u['mot_de_passe'];
         echo 'connexion reussie';
+        header('Location:views/accueil.php');
+        exit();
     } else {
         $_SESSION['message'] = "Erreur";
-        header('Location:./views/404.php');
+        header('Location:views/404.php');
         exit;
     }
     }

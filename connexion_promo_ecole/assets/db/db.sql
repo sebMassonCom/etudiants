5f23eeebea8f3b0b608cf5d33975db8a684f0d90
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-05-28 08:50:03 +0200

    push db

--- connexion_promo_ecole/assets/db/db.sql
+++ connexion_promo_ecole/assets/db/db.sql
@@ -4,10 +4,24 @@ USE espace_etudiant;
 
 CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    ecole INT,
-    promotion INT,
+    id_ecole INT,
+    id_promotion INT,
     nom VARCHAR(255),
-    mot_de_passe VARCHAR(100)
+    mot_de_passe VARCHAR(100),
+    FOREIGN KEY (id_ecole) REFERENCES ecole(id),
+    FOREIGN KEY (id_promotion) REFERENCES promotion(id)
+    
+);
+
+CREATE TABLE admin(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_ecole INT,
+    id_promotion INT,
+    nom VARCHAR(255),
+    mot_de_passe VARCHAR(100),
+    FOREIGN KEY (id_ecole) REFERENCES ecole(id),
+    FOREIGN KEY (id_promotion) REFERENCES promotion(id)
+    
 );
 
 CREATE TABLE promotion(

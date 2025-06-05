720eeb53c89f1289dea457aea1d9fe1e6c95a5a1
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-06-05 16:51:35 +0200

    push db.sql

--- connexion_promo_ecole/assets/db/db.sql
+++ connexion_promo_ecole/assets/db/db.sql
@@ -2,31 +2,30 @@ CREATE DATABASE espace_etudiant;
 
 USE espace_etudiant;
 
-CREATE TABLE users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_ecole INT,
-    id_promotion INT,
-    nom VARCHAR(255),
-    mot_de_passe VARCHAR(100),
-    FOREIGN KEY (id_ecole) REFERENCES ecole(id),
-    FOREIGN KEY (id_promotion) REFERENCES promotion(id)
-    
+
+CREATE  TABLE categorie(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom_categorie varchar(255)
 );
 
-CREATE TABLE admin(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_ecole INT,
+CREATE TABLE Fichier_rendu (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     id_promotion INT,
-    nom VARCHAR(255),
-    mot_de_passe VARCHAR(100),
-    FOREIGN KEY (id_ecole) REFERENCES ecole(id),
-    FOREIGN KEY (id_promotion) REFERENCES promotion(id)
-    
+    nom_fichier VARCHAR(255) NOT NULL,
+    taille INT,
+    date_rendu DATETIME,
+    id_categorie INT,
+    FOREIGN KEY (id_promotion) REFERENCES promotion(id),
+    FOREIGN KEY (id_categorie) REFERENCES categorie(id)
 );
 
 CREATE TABLE promotion(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_promotion varchar(255)
+    id_ecole INT,
+    nom_promotion VARCHAR(255),
+    mot_de_passe VARCHAR(100),
+    FOREIGN KEY (id_ecole) REFERENCES ecole(id)
+  
 );
 
 CREATE TABLE ecole(

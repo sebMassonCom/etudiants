22a15d462afb85ff3eef2eebbcd7c430f2c46718
Raphaelmartinuzzi <raphael.martinuzzi910@gmail.com>
2025-07-02 16:40:20 +0200

    push models

--- connexion_promo_ecole/models/model_Admin.php
+++ connexion_promo_ecole/models/model_Admin.php
@@ -4,10 +4,7 @@ require_once 'core/Database.php';
 
 class Model_Admin{
 
-    // private $_sql;
-
     public function __construct() {
-        // $this->_sql = new Database();
         new Database; 
     }
 
@@ -16,9 +13,8 @@ class Model_Admin{
     }
 
     public function creer_classe($ecole, $nom , $mdp){
-        // $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
+        $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
 
-        
         $sql = Database::$db->prepare("INSERT INTO promotion(id_ecole,nom_promotion, mot_de_passe) VALUES (:id_ecole,:nom_promotion ,:mot_de_passe)");
         $sql->bindParam(':id_ecole', $ecole);
         $sql->bindParam(':nom_promotion', $nom);
@@ -27,27 +23,84 @@ class Model_Admin{
         
     }
 
-    // public function creer_mdp($mdp){
-    //     $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
+    public function creer_mdp($mdp){
+        $hashed_password = password_hash($mdp, PASSWORD_BCRYPT);
 
-    //     $sql = Database::$db->prepare("INSERT INTO promotion(mot_de_passe) VALUES (:mot_de_passe)");
-    //     $sql->bindParam(':mot_de_passe', $hashed_password);
-    //     $sql->execute();
-    // }
+        $sql = Database::$db->prepare("INSERT INTO promotion(mot_de_passe) VALUES (:mot_de_passe)");
+        $sql->bindParam(':mot_de_passe', $hashed_password);
+        $sql->execute();
+    }
 
 
     public function deposer_fichier($nom_fichier, $taille, $date_rendu, $id_promotion, $id_categorie) {
-        
-        
+    
         $sql = Database::$db->prepare("INSERT INTO fichier_rendu(nom_fichier, taille, date_rendu, id_promotion, id_categorie) VALUES (:nom_fichier, :taille, :date_rendu, :id_promotion, :id_categorie)");
-        
         $sql->bindParam(':nom_fichier', $nom_fichier);
         $sql->bindParam(':taille', $taille);
         $sql->bindParam(':date_rendu', $date_rendu);
         $sql->bindParam(':id_promotion', $id_promotion);
-        $sql->bindParam(':id_promotion', $id_promotion);
         $sql->bindParam(':id_categorie', $id_categorie);
-
         $sql->execute();
     }
+    
+    public function recup_categorie(){
+        return Database::$db->query("SELECT * FROM categorie");
+    }
+
+    public function recup_promotion(){
+        return Database::$db->query("SELECT * FROM promotion");
+    }
+
+   public function recup_fichier(){
+        return Database::$db->query("SELECT * FROM fichier_rendu ORDER BY date_rendu DESC");
+    }
+
+    public function recup_fichier_filtre($id_promotion, $id_categorie){
+        if ($id_categorie == null) {
+            $sql = Database::$db->prepare("SELECT * FROM fichier_rendu WHERE id_promotion = :id_promotion ORDER BY date_rendu DESC");
+            $sql->bindParam(':id_promotion', $id_promotion);
+        } else {
+            $sql = Database::$db->prepare("SELECT * FROM fichier_rendu WHERE id_promotion = :id_promotion AND id_categorie = :id_categorie ORDER BY date_rendu DESC");
+            $sql->bindParam(':id_promotion', $id_promotion);
+            $sql->bindParam(':id_categorie', $id_categorie);
+        }
+        $sql->execute();
+        return $sql;
+    }
+
+    public function supprimer_fichier($id){
+        $sql = Database::$db->query("DELETE FROM fichier_rendu WHERE id = $id");
+        return $sql;
+    }
+
+    public function activer_examen(int $id_promotion) {
+        // interroger la base de donnée pour connaitre l'etat du champ examen et du champ promotion
+        $sql = Database::$db->query("SELECT * FROM promotion WHERE id = $id_promotion LIMIT 1");
+        // $sql->bindParam(':id_promotion', $id_promotion);
+        // $reponse = $sql->execute();
+        // foreach($sql AS $r){
+        //     var_dump($r); exit();
+        // }
+        $reponse = $sql->fetch();
+        // var_dump($reponse); exit();
+        // si le champ vaut 0 : 
+        if ($reponse["examen_actif"] == 0){
+            $position = 1;
+        }else {
+            $position = 0;
+        }
+        // var_dump($position); exit();
+        $sql = Database::$db->prepare("UPDATE promotion SET examen_actif = $position WHERE id = :id_promotion");
+        $sql->bindParam(':id_promotion', $id_promotion);
+        $sql->execute();
+    }
+
+    public function recup_fichier_examen() {
+        return Database::$db->query("SELECT * FROM examen ORDER BY date_envoie DESC");
+    }
 }
+
+
+
+
+
--- connexion_promo_ecole/models/model_Utilisateur.php
+++ connexion_promo_ecole/models/model_Utilisateur.php
@@ -2,36 +2,78 @@
 
 require_once 'core/Database.php';
 
-class Model_Utilisateur{
-
-    // private $_sql;
+class Model_Utilisateur {
 
     public function __construct() {
-        // $this->_sql = new Database();
         new Database;
     }
 
-    public function recup_ecole(){
+    public function recup_ecole() {
         return Database::$db->query("SELECT * FROM ecole"); 
     }
 
-    public function afficher_nom_ecole($ecole){
-        return Database::$db->query("SELECT * FROM promotion INNER JOIN id_ecole ON promotion.id_ecole = ecole.id");
+    public function afficher_nom_ecole() {
+        return Database::$db->query("SELECT * FROM promotion INNER JOIN ecole ON promotion.id_ecole = ecole.id");
+    }
+
+    public function afficher_nom_classe(Promotion $nom) {
+        $n = $nom->get_nom();
+        $football = Database::$db->prepare("SELECT * FROM promotion WHERE nom_promotion = :noham");
+        $football->bindParam(':noham', $n);
+        $football->execute();
+        return $football;
 
     }
 
-    public function afficher_nom_classe($nom){
-        return Database::$db->query("SELECT * FROM promotion INNER JOIN nom ON promotion.id_nom = nom.id");
-
+    public function verifier_mdp() {
+        return Database::$db->query("SELECT * FROM promotion");
     }
 
-    public function verifier_mdp($mdp){
-        $sql = Database::$db->query("SELECT * FROM promotion WHERE nom='$mdp' LIMIT 1");
+    public function recup_categorie(){
+        return Database::$db->query("SELECT * FROM categorie");
+    }
+
+    public function recup_promotion(){
+        return Database::$db->query("SELECT * FROM promotion");
+    }
+
+
+    public function recup_fichier(){
+        return Database::$db->query("SELECT * FROM fichier_rendu");
+    }
+
+    public function recup_fichier_filtre($id_promotion, $id_categorie) {
+    if ($id_categorie == null) {
+        $sql = Database::$db->prepare("SELECT * FROM fichier_rendu WHERE id_promotion = :id_promotion ORDER BY date_rendu DESC");
+        $sql->bindParam(':id_promotion', $id_promotion);
+    } else {
+        $sql = Database::$db->prepare("SELECT * FROM fichier_rendu WHERE id_promotion = :id_promotion AND id_categorie = :id_categorie ORDER BY date_rendu DESC");
+        $sql->bindParam(':id_promotion', $id_promotion);
+        $sql->bindParam(':id_categorie', $id_categorie);
+    }
+    $sql->execute();
+    return $sql;
+    }
+
+    public function recupCategorieExamen() {
+        $sql = Database::$db->prepare("SELECT * FROM categorie WHERE LOWER(nom_categorie) = 'examen'");
         $sql->execute();
- 
+        return $sql->fetch();
     }
-    public function recup_dossier(){
-    return Database::$db->query("SELECT * FROM fichier_rendu GROUP BY id_promotion");
-    
+
+    public function examenEstActif($id_promotion) {
+        $sql = Database::$db->prepare("SELECT examen_actif FROM promotion WHERE id = :id_promotion");
+        $sql->bindParam(':id_promotion', $id_promotion);
+        $sql->execute();
+        $recup = $sql->fetch();
+        return $recup && $recup['examen_actif'] == 1;
+    }
+    public function insererExamen($id_promotion, $nom_fichier, $taille, $date) {
+        $sql = Database::$db->prepare("INSERT INTO examen (id_promotion, nom_fichier, taille, date_envoie) VALUES (:id_promotion, :nom_fichier, :taille, :date_envoie)");
+        $sql->bindParam(':id_promotion', $id_promotion);
+        $sql->bindParam(':nom_fichier', $nom_fichier);
+        $sql->bindParam(':taille', $taille);
+        $sql->bindParam(':date_envoie', $date);
+        $sql->execute();
     }
 }
